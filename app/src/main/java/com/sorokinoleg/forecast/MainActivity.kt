8b25c3de42a092fe8d69ab20401238b4cac24cc0
unbleaklessness <sorokin.oleg.edu@gmail.com>
2019-03-08 15:41:32 +0300

    Added 3 main fragments

--- app/src/main/java/com/sorokinoleg/forecast/MainActivity.kt
+++ app/src/main/java/com/sorokinoleg/forecast/MainActivity.kt
@@ -2,12 +2,43 @@ package com.sorokinoleg.forecast
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val fragmentContainer = R.id.container
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showFragment(TodayFragment())
+
+        bottomNavigationView.setOnNavigationItemSelectedListener OnNavigationItemSelectedListener@{
+            when (it.itemId) {
+                R.id.navigationToday -> {
+                    showFragment(TodayFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigationWeek -> {
+                    showFragment(WeekFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigationSettings -> {
+                    showFragment(SettingsFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+
+            }
+            false
+        }
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(fragmentContainer, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
